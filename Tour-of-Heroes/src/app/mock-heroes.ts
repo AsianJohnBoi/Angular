4ffe7ae561bacd0ae051d5487cc1758cfae4f522
AsianJohnBoi <Jbssantias@hotmail.com>
2019-04-24 12:07:46 +1000

    displayed lists of heroes

---
+++ Tour-of-Heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Spiderman'},
+    {id: 12, name: 'Thor'},
+    {id: 13, name: 'Captain America'},
+    {id: 14, name: 'Iron Man'},
+    {id: 15, name: 'Black Widow'},
+    {id: 16, name: 'Hulk'},
+    {id: 17, name: 'Black Panther'},
+    {id: 18, name: 'Dr. Strange'},
+    {id: 19, name: 'Hawkeye'},
+    {id: 20, name: 'Captain Marvel'},
+];
